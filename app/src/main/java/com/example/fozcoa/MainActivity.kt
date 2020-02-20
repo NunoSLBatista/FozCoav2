4b52dc830592297a29909542c41cf100163e687b
NunoSLBatista <nuno.s.lima.batista@gmail.com>
2020-02-20 22:38:51 +0000

    Teste incial de commit

--- app/src/main/java/com/example/fozcoa/MainActivity.kt
+++ app/src/main/java/com/example/fozcoa/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+
+    //Did it again
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
