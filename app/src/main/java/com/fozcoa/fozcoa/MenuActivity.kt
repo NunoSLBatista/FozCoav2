cb6315ef3744a36e7d581328cdbd3031c9b53857
Nuno <nuno.s.lima.batista@gmail.com>
2020-12-15 19:59:21 +0000

    Login e registo alterações | Logo

--- app/src/main/java/com/fozcoa/fozcoa/MenuActivity.kt
+++ app/src/main/java/com/fozcoa/fozcoa/MenuActivity.kt
@@ -23,24 +23,6 @@ import java.util.*
 
 class MenuActivity : AppCompatActivity() {
 
-    // declare a global variable FusedLocationProviderClient
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
-
-
-    // globally declare LocationRequest
-    private lateinit var locationRequest: LocationRequest
-
-    // globally declare LocationCallback
-    private lateinit var locationCallback: LocationCallback
-
-    private var locationManager: LocationManager? = null
-
-    private val isLocationEnabled: Boolean
-        get() {
-            locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-            return locationManager!!.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager!!.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-        }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
@@ -97,60 +79,6 @@ class MenuActivity : AppCompatActivity() {
         startActivity(goLogin)
     }
 
-    //start location updates
-    private fun startLocationUpdates() {
-        fusedLocationClient.requestLocationUpdates(
-            locationRequest,
-            locationCallback,
-            null /* Looper */
-        )
-    }
-
-    // stop location updates
-    private fun stopLocationUpdates() {
-        fusedLocationClient.removeLocationUpdates(locationCallback)
-    }
-
-    fun getLastKnownLocation() {
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener { location->
-                if (location != null) {
-                    // use your location object
-                    // get latitude , longitude and other info from this
-                    Log.d("latitude", location.latitude.toString())
-                    Log.d("longitude", location.longitude.toString())
-                    val gcd : Geocoder = Geocoder(applicationContext, Locale.getDefault())
-                    val addresses = gcd.getFromLocation(location.latitude, location.longitude, 1)
-                    if(addresses.size > 0){
-                        Log.d("Country", addresses.get(0).countryName)
-                    }
-                }else {
-                    Log.d("no", "location")
-                }
-
-            }
-
-    }
-
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
-                                            grantResults: IntArray) {
-        when (requestCode) {
-            1 -> {
-                if (grantResults.isNotEmpty() && grantResults[0] ==
-                    PackageManager.PERMISSION_GRANTED) {
-                    if ((ContextCompat.checkSelfPermission(this@MenuActivity,
-                            Manifest.permission.ACCESS_FINE_LOCATION) ===
-                                PackageManager.PERMISSION_GRANTED)) {
-                        Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
-                    }
-                } else {
-                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
-                }
-                return
-            }
-        }
-    }
 
 
 }
